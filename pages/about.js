e895fb2b93baa9a2ad3ac229f03f26348a93bc3e
itc-s22001 <s22001@std.it-college.ac.jp>
2023-02-14 07:36:08 +0000

    2.7 about, blog

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Hero from 'components/hero'
+
+const About = () => {
+  return (
+    <Hero />
+  )
+}
+export default About
---
+++ pages/blog/index.js
@@ -0,0 +1,8 @@
+import Hero from 'components/hero'
+
+const Blog = () => {
+  return (
+    <Hero />
+  )
+}
+export default Blog
