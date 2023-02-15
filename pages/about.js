87bce74a5dd85066e47ed9f2e659890cd901746e
itc-s22001 <s22001@std.it-college.ac.jp>
2023-02-15 06:16:06 +0000

    4.7 Contact作成

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
+import Contact from 'components/contact'
 
 const About = () => {
   return (
@@ -29,6 +30,7 @@ const About = () => {
           くりは、これからも続けていきたいです。
         </p>
       </PostBody>
+      <Contact />
     </Container>
   )
 }
