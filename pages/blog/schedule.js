6d55fae639f7a6cb0a81702168760a1c4c33b4c9
itc-s22001 <s22001@std.it-college.ac.jp>
2023-02-16 05:27:21 +0000

    記事ページに必要なデータを取得する関数の用意

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,23 +1,25 @@
-import { client } from 'lib/api'
+import { getPostBySlug } from 'lib/api'
+import Container from 'components/container'
 
-const Schedule = () => {
+const Schedule = ({ title, publish, content, eyecatch, categories }) => {
   return (
-    <h1>記事のタイトル</h1>
+    <Container>
+      <h1>{title}</h1>
+    </Container>
   )
 }
 export async function getStaticProps () {
-  const resPromise = client.get({
-    endpoint: 'blogs'
-  })
-  try {
-    const res = await resPromise
-    console.log(res)
-  } catch (err) {
-    console.log(err)
-  }
+  const slug = 'schedule'
+  const post = await getPostBySlug(slug)
 
   return {
-    props: {}
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories
+    }
   }
 }
 export default Schedule
