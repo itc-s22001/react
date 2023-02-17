4014dfafcd16a106ec423cbea646ae8b8029348f
itc-s22001 <s22001@std.it-college.ac.jp>
2023-02-17 05:24:17 +0000

    9.6 記事一覧にnext/imageでアイキャッチ画像を追加

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,9 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
 
 const Blog = ({ posts }) => {
   return (
@@ -15,6 +18,13 @@ const Blog = ({ posts }) => {
 }
 export async function getStaticProps () {
   const posts = await getAllPosts()
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
   return {
     props: {
       posts
